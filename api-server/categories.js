9a78373b81e9ec442461acbf425960a76052aa81
NewMediaCatalyst <newcatalystweb@gmail.com>
2017-10-23 17:10:26 -0700

    chore: Update categories

--- api-server/categories.js
+++ api-server/categories.js
@@ -1,41 +1,49 @@
-const clone = require('clone')
-const config = require('./config')
+const clone = require('clone');
+const config = require('./config');
 
-let db = {}
+let db = {};
 
 const defaultData = {
   categories: [
       {
-        name: 'react',
-        path: 'react'
+        name: 'bitcoin',
+        path: 'bitcoin',
+        ticker: 'btc'
       },
       {
-        name: 'redux',
-        path: 'redux'
+        name: 'ethereum',
+        path: 'ethereum',
+        ticker: 'eth'
       },
       {
-        name: 'udacity',
-        path: 'udacity'
+        name: 'ripple',
+        path: 'ripple',
+        ticker: 'xrp'
+      },
+      {
+        name: 'litecoin',
+        path: 'litecoin',
+        ticker: 'ltc'
       }
   ]
 }
 
 function getData (token) {
-  //Each token has it's own copy of the DB. The token in this case is like an app id.
-  let data = db[token]
-  //This populates the default user data if there isn't any in the db.
-  if (data == null) {
-    data = db[token] = clone(defaultData)
-  }
-  return data
+    //Each token has it's own copy of the DB. The token in this case is like an app id.
+    let data = db[token];
+    //This populates the default user data if there isn't any in the db.
+    if (data == null) {
+        data = db[token] = clone(defaultData);
+    }
+    return data;
 }
 
 function getAll (token) {
-  return new Promise((res) => {
-    res(getData(token))
-  })
+    return new Promise((res) => {
+        res(getData(token));
+    })
 }
 
 module.exports = {
-  getAll
+    getAll
 }
